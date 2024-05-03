da59eed19467efc6d7ee549a03f9b630df09d84d
Apologranzze <104176563+Apologranzze@users.noreply.github.com>
2024-05-03 19:38:00 +0800

    Update ucs.py

--- algo/ucs.py
+++ algo/ucs.py
@@ -1,5 +1,6 @@
 import heapq
 
+
 class Node:
     def __init__(self, state, cost, total_cost):
         self.state = state
@@ -9,9 +10,19 @@ class Node:
     def __lt__(self, other):
         return self.total_cost < other.total_cost
 
-def ucs(graph, start, goal):
+
+def ucs(start_node, goal_node):
+    graph = {
+                'S': {'A': 3, 'B': 2, 'C': 6},
+                'A': {'S': 3, 'B': 1, 'D': 1},
+                'B': {'S': 2, 'A': 1, 'D': 4, 'E': 2},
+                'C': {'S': 6, 'E': 5},
+                'D': {'A': 1, 'B': 4, 'E': 1, 'F': 5},
+                'E': {'B': 2, 'C': 5, 'D': 1, 'F': 3},
+                'F': {'D': 5, 'E': 3}
+            }
     frontier = []
-    heapq.heappush(frontier, Node(start, 0, 0))
+    heapq.heappush(frontier, Node(start_node, 0, 0))
     explored = set()
 
     while frontier:
@@ -22,7 +33,7 @@ def ucs(graph, start, goal):
         print("Current cost:", curr_Cost)
         print("Current total cost:", curr_TCost)
 
-        if curr_State == goal:
+        if curr_State == goal_node:
             print("Goal reached! Path found:", curr_State, "Total cost:", curr_TCost)
             return curr_State, curr_TCost  # Goal reached
 
@@ -33,24 +44,9 @@ def ucs(graph, start, goal):
                 total_cost = curr_TCost + cost
                 heapq.heappush(frontier, Node(neighbor, cost, total_cost))
 
-    print("Goal not reachable.")
-    return None, None
-
-graph = {
-    'S': {'A': 3, 'B': 2, 'C': 6},
-    'A': {'S': 3, 'B': 1, 'D': 1},
-    'B': {'S': 2, 'A': 1, 'D': 4, 'E': 2},
-    'C': {'S': 6, 'E': 5},
-    'D': {'A': 1, 'B': 4, 'E': 1, 'F': 5},
-    'E': {'B': 2, 'C': 5, 'D': 1, 'F': 3},
-    'F': {'D': 5, 'E': 3}
-}
-
-start_node = 'S'
-goal_node = 'F'
-
-result_node, total_cost = ucs(graph, start_node, goal_node)
-if result_node:
-    print("Goal reached! Path found:", result_node, "Total cost:", total_cost)
-else:
-    print("Goal not reachable.")
+    result_node, total_cost = ucs(start_node, goal_node)
+    if result_node:
+        print("Goal reached! Path found:", result_node, "Total cost:", total_cost)
+    else:
+        print("Goal not reachable.")
+        return None, None
